be40475daf938c03ed56a72fa52bba076adc8397
Nick Malcolm <nick@southgatelabs.com>
2011-07-20 21:27:02 +1200

    Gah

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,15 +8,6 @@ class HomeController < ApplicationController
   end
   
   def index
-<<<<<<< HEAD
-    
-=======
-    # get 3 products
-    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 3})
-
-    # get latest 3 orders
-    @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 3, :order => "created_at DESC" })
->>>>>>> 5790ba56e1249a531d6d5675d8999e5c91129dfb
   end
   
 end
