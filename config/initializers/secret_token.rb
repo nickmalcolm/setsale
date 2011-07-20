bab9afe4b1b9919f76377105b7e2227931374e82
Nick Malcolm <nick@southgatelabs.com>
2011-07-20 20:38:33 +1200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Setsale::Application.config.secret_token = '280d8759ac0d9fc8c392d5007a8e7f604aa7b03a09261155537887d949b6c583bd65e5d1822bcc2a28739f114464cdc714895d5c606255123de1d38572fad3b4'
