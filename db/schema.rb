b509bdcc4eb3d52fadbb2088d0186bce8bcb05a2
Nick Malcolm <nick@southgatelabs.com>
2011-07-22 23:19:28 +1200

    Adds amount type to sales

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110722110930) do
+ActiveRecord::Schema.define(:version => 20110722111902) do
 
   create_table "products", :force => true do |t|
     t.string   "handle"
@@ -29,7 +29,8 @@ ActiveRecord::Schema.define(:version => 20110722110930) do
     t.string   "name"
     t.datetime "starts_at"
     t.datetime "ends_at"
-    t.decimal  "amount",     :precision => 16, :scale => 2, :default => 0.0
+    t.decimal  "amount",      :precision => 16, :scale => 2, :default => 0.0
+    t.string   "amount_type"
   end
 
   create_table "shops", :force => true do |t|
