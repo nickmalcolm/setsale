6c3f0e95082c75b9878c437974dbaf6a7a960665
Nick Malcolm <nick@southgatelabs.com>
2011-07-22 23:37:43 +1200

    Extracts amount_types into class constant

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,9 +1,11 @@
 class Sale < ActiveRecord::Base
   
+  AMOUNT_TYPES = %w(fixed percentage)
+  
   validates_presence_of :starts_at
   validates_presence_of :ends_at
   validate :amount, :presence => true, :numericality => true, :allow_nil => false
-  validates_inclusion_of :amount_type, :in => %w(fixed percentage)
+  validates_inclusion_of :amount_type, :in => AMOUNT_TYPES
   
   attr_accessible :starts_at, :ends_at, :amount, :amount_type
   
